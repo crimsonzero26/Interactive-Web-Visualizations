5aa3335ad937a6b4d4f8d220107142e2c388358d
Martin Vazquez <mvredfalcon26@gmail.com>
2022-10-02 18:54:09 -0500

    Metadata added, started gauge

--- static/js/app.js
+++ static/js/app.js
@@ -91,11 +91,60 @@ function DrawBubblechart(sampleId)
 function DrawGauge(sampleId)
 {
     console.log(`DrawGauge(${sampleId})`);
+
+    d3.json(url).then(data => {
+
+        let wash_freq = data.WFREQ;
+        console.log(wash_freq);
+  
+        var data = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: wash_freq,
+          title: { text: "<b>Wash Frequency</b><br><i>Scrubs per week</i>"},
+          type: "indicator",
+          mode: "gauge+number+range",
+          gauge: {
+            axis: { range: [0, 10] },
+            bar: {color: "darkblue"},
+            steps: [
+              { range: [0, 1], color: "rgb(204,214,204)" },
+              { range: [1, 2], color: "rgb(186,206,186)" },
+              { range: [2, 3], color: "rgb(168,199,168)" },
+              { range: [3, 4], color: "rgb(150,191,150)" },
+              { range: [4, 5], color: "rgb(132,184,132)" },
+              { range: [5, 6], color: "rgb(114,176,114)" },
+              { range: [6, 7], color: "rgb(96,168,96)" },
+              { range: [7, 8], color: "rgb(78,161,78)" },
+              { range: [8, 9], color: "rgb(60,153,60)" },
+              { range: [9, 10], color: "rgb(42,146,42)" }
+            ],
+            }
+        }
+        ]
+    });
 }
 
 function ShowMetadata(sampleId)
 {
     console.log(`ShowMetadata(${sampleId})`);
+
+    d3.json(url).then((data) => {
+        let metadata = data.metadata;
+        console.log(metadata);
+
+        // Filter data
+        let result = metadata.filter(meta => meta.id == sampleId)[0];
+        let demographicInfo = d3.select('#sample-metadata');
+
+        // Clear existing data in demographicInfo
+        demographicInfo.html('');
+
+        // Add key and value pair to the demographicInfo panel
+        Object.entries(result).forEach(([key, value]) => {
+            demographicInfo.append('h6').text(`${key}: ${value}`);
+        });
+    });
 }
 
 function optionChanged(sampleId)
